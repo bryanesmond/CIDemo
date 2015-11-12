ef91aa612162b8997f653dc2af6976cdc91985fa
Bryan Esmond <BEsmond@shelterinsurance.com>
2015-11-12 12:12:33 -0600

    updated blog

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -3,11 +3,6 @@ class Blog extends CI_Controller {
 
         public function index()
         {
-                echo 'Hello World!';
-        }
-
-        public function comments()
-        {
-                echo 'Look at this!';
-        }
+              $this->load->view('blogview');
 }
+?>
